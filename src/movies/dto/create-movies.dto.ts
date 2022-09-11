75ed14079f519f017dbe222741c01b4fdfad072d
mahwin <mahwin7085@gmail.com>
2022-09-11 14:47:55 +0900

    Create update-movie.dto

--- src/movies/dto/create-movies.dto.ts
+++ src/movies/dto/create-movies.dto.ts
@@ -1,10 +1,12 @@
-import { IsString, isNumber } from 'class-validator';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
   readonly title: string;
-  @isNumber()
+  @IsNumber()
   readonly year: number;
-  @IsString()
+
+  @IsOptional()
+  @IsString({ each: true })
   readonly genres: string[];
 }
