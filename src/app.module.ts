b2874326f0fdfcd07e788b880cff07bee9d461aa
mahwin <mahwin7085@gmail.com>
2022-09-11 17:45:30 +0900

    Feat movies.service e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app/app.controller';
+import { AppController } from './app.controller';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
