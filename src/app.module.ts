3fd935550b68b582870ea18fbd70ce1b9036e805
mahwin <mahwin7085@gmail.com>
2022-09-11 15:52:46 +0900

    Seperate app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { AppController } from './app/app.controller';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
-  //url을 가져오고 함수를 실행하는 역할. express 의 라우터.
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
